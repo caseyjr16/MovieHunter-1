d0e0aa9f05eedcdbf2a9cf090f5ef4d35b3f32d4
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-16 11:40:22 -0400

    Initial Commit of Spring Boot project.

---
+++ java/com/MovieHunter/MovieHunter/MovieHunterApplication.java
@@ -0,0 +1,13 @@
+package com.MovieHunter.MovieHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHunterApplication.class, args);
+	}
+
+}
