fd77b1a1612ec41da2cd66e003044522e1715e4a
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-16 12:02:38 -0400

    Created movie hunter controller

--- java/com/MovieHunter/MovieHunterApplication.java
+++ java/com/MovieHunter/MovieHunterApplication.java
@@ -1,4 +1,4 @@
-package com.MovieHunter.MovieHunter;
+package com.MovieHunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
